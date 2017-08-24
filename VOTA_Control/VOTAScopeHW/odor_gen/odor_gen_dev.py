-fixed a bug in vota_sniff, corrected camera rotation

VOTA_Control/VOTAScopeHW/odor_gen/odor_gen_dev.py
@@ -8,7 +8,7 @@ from scipy import signal,pi
 from queue import Queue
 import matplotlib.pyplot as plt
 from random import random,randint
-
+from math import sqrt
 class OdorGenDev(object):
     '''
     classdocs
@@ -67,14 +67,15 @@ class OdorGenDev(object):
             for i in range(on_pulse_ms):
                 self.buffer.put([data[i,:].astype(int).squeeze().tolist(),disp_data[i,:].astype(int).squeeze().tolist()])
                 
-    def pulse(self,sol,pulse_ms,on_ms,pre_pulse_ms,sol_level):
+    def pulse(self,sol,pulse_ms,on_ms,pre_pulse_ms,sol_level,clean_factor):
         data=np.zeros((pulse_ms,self.num_of_sol),dtype=float)
         disp_data=np.zeros((pulse_ms,self.num_of_sol),dtype=float)
         data[0:on_ms,sol]=sol_level
         disp_data[0:on_ms:,sol]=sol_level
         data[0:pre_pulse_ms,sol]=self.preload_level
-        data[0:on_ms,0]=100-sol_level
-        disp_data[0:on_ms,0]=100-sol_level
+        sol_factor=(sol_level/100.0)*(sol_level/100.0)
+        data[0:on_ms,0]=100-sol_level*sol_factor*clean_factor
+        disp_data[0:on_ms,0]=100-sol_level*sol_factor*clean_factor
         data[on_ms:pulse_ms,0]=100
         disp_data[on_ms:pulse_ms,0]=100
         for i in range(pulse_ms):